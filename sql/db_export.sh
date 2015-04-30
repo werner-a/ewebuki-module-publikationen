91acba795936e597cb661cc07434132e1230221c
werner-a <chaot@chaos.de>
2015-04-30 14:41:57 +0200

    more tricks

--- sql/db_export.sh
+++ sql/db_export.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 pg_dump --username=ewebuki --table=db_publikationen_id_seq --table=db_publikationen -xO --column-inserts --inserts energieatlas > db_publikationen.dump
+psql --username=ewebuki --dbname=energieatlas -c "copy(select * from site_text where label like 'pb_%')TO STDOUT" > site_text.csv
--- sql/db_import.sh
+++ sql/db_import.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 psql --username=ewebuki -f db_publikationen.dump energieatlas
+psql --username=ewebuki --dbname=energieatlas -c "delete from site_text where label like 'pb_%';copy site_text FROM STDIN" < site_text.csv
